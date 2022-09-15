c89b02b4def97779852434b89609a3715c3ffc29
stellaris9950 <andyzihengwei@gmail.com>
2022-09-15 13:28:18 -0600

    SHELL CREATE

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // Triangle Perimeter Assignment Start Code
+// link button to function
+document.getElementById("button").addEventListener("click", calculate);
+
+// function of calculating
+function calculate(){
+  // getting number
+  let AC = +document.getElementById("AC").value;
+  let BC = +document.getElementById("BC").value;
+  let AB = +document.getElementById("AB").value;
+  // process
+  let output = AC + BC + AB ; 
+  // output
+  document.getElementById("output").innerHTML = output;
+}
